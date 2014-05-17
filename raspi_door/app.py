3270ad28460d7d7c314d852bfcf98538c2d01d83
metagriffin <mg.github@uberdev.org>
2014-05-17 18:54:48 -0400

    added initial mock camera integration

--- raspi_door/app.py
+++ raspi_door/app.py
@@ -23,9 +23,63 @@ import os
 import wx, wx.xrc
 from aadict import aadict
 import morph
+import six
+import threading
+import time
 
 from .i18n import _
-from . import lock
+from . import lock, camera
+
+#------------------------------------------------------------------------------
+class CameraCaptureThread(threading.Thread):
+
+  # #----------------------------------------------------------------------------
+  # class CaptureEvent(wx.PyEvent):
+  #   ID = wx.NewId()
+  #   def __init__(self, bitmap):
+  #     wx.PyEvent.__init__(self)
+  #     self.SetEventType(self.ID)
+  #     self.bitmap = bitmap
+
+  #----------------------------------------------------------------------------
+  def __init__(self, app, fps=10, *args, **kw):
+    super(CameraCaptureThread, self).__init__(*args, **kw)
+    self.app      = app
+    self._running = False
+    self._cond    = threading.Condition()
+    self._timeout = 1.0 / float(self.app.getConfig('camera.fps', 10))
+    self._buf     = six.BytesIO()
+
+  #----------------------------------------------------------------------------
+  @property
+  def running(self):
+    with self._cond:
+      return self._running
+  @running.setter
+  def running(self, value):
+    with self._cond:
+      self._running = value
+      self._cond.notify()
+
+  #----------------------------------------------------------------------------
+  def run(self):
+    while True:
+      with self._cond:
+        if not self._running:
+          self._cond.wait()
+          continue
+        self.capture()
+        self._cond.wait(self._timeout)
+
+  #----------------------------------------------------------------------------
+  def capture(self):
+    # todo: make this `resize` depend on the screen...
+    self._buf.seek(0)
+    self.app.camera.capture(self._buf, format='png', resize=(240, 180))
+    self._buf.seek(0)
+    bmp = wx.BitmapFromImage(wx.ImageFromStream(self._buf))
+    # wx.PostEvent(self.app.dispimg, CameraCaptureThread.CaptureEvent(bmp))
+    wx.CallAfter(self.app.onCapture, aadict(bitmap=bmp))
 
 #------------------------------------------------------------------------------
 class App(wx.App):
@@ -45,13 +99,20 @@ class App(wx.App):
     # start resources
     # todo: check & use self.options.remote...
     self.lock    = lock.MockLock() if self.options.mock else lock.RaspiLock()
-    self.camera  = None # todo
+    self.camera  = camera.MockCamera() if self.options.mock else camera.RaspiCamera() # todo
     self.weather = None # todo
     super(App, self).__init__(*args, **kw)
 
   #----------------------------------------------------------------------------
   def start(self):
     self.MainLoop()
+
+  #----------------------------------------------------------------------------
+  def getConfig(self, option, default=None, section='gui'):
+    try:
+      return self.options.config.get(section, option)
+    except Exception as err:
+      return default
 
   #----------------------------------------------------------------------------
   def OnInit(self):
@@ -71,6 +132,7 @@ class App(wx.App):
     self.main.Show()
     self.SetTopWindow(self.main)
     self.initDisplay()
+    self.initReminder()
     self.initTime()
     self.initWeather()
     self.initButtons()
@@ -83,12 +145,32 @@ class App(wx.App):
     self.disptext  = wx.xrc.XRCCTRL(self.main, 'DisplayText')
     self.dispimg   = wx.xrc.XRCCTRL(self.main, 'DisplayImage')
     self.lock.change.subscribe(self.lockChanged)
+    self.disp_thread = CameraCaptureThread(self)
+    self.disp_thread.daemon = True
+    self.disp_thread.start()
+    # self.dispimg.Connect(-1, -1, CameraCaptureThread.CaptureEvent.ID, self.onCapture)
+
+  #----------------------------------------------------------------------------
+  def onCapture(self, event):
+    # todo: perhaps check to make sure that the camera is visible?
+    # TODO: check to see if we are falling behind and, if so, drop the frame
+    self.dispimg.SetBitmap(event.bitmap)
+
+  #----------------------------------------------------------------------------
+  def toggleDisplay(self, event):
+    self.showDisplayCamera(self.disptext.Shown)
+
+  #----------------------------------------------------------------------------
+  def initReminder(self):
+    # TODO
+    self.rembox = wx.xrc.XRCCTRL(self.main, 'ReminderText')
+    self.rembox.SetLabel('to 10:30PM: max/rakesh for dinner')
 
   #----------------------------------------------------------------------------
   def initTime(self):
     # TODO
     self.timebox = wx.xrc.XRCCTRL(self.main, 'NowTimeText')
-    self.timebox.SetLabel('9:35')
+    self.timebox.SetLabel('9:35 pm')
 
   #----------------------------------------------------------------------------
   def initWeather(self):
@@ -111,35 +193,14 @@ class App(wx.App):
     self.btnunlock.Bind(wx.EVT_LEFT_UP, self.do_unlock)
     self.btnlock.Bind(wx.EVT_LEFT_UP, self.do_lock)
     self.btnplock.Bind(wx.EVT_LEFT_UP, self.do_plock)
-    self.btninfo.Bind(wx.EVT_LEFT_UP, self.toggleInfo)
+    self.btninfo.Bind(wx.EVT_LEFT_UP, self.toggleDisplay)
 
   #----------------------------------------------------------------------------
-  def toggleInfo(self, event):
-    self.showDisplayText(not self.disptext.Shown)
-
-  #----------------------------------------------------------------------------
-  def showDisplayText(self, show):
-    # self.disptext.SetHidden(not self.disptext.IsHidden())
-    # self.dispimg.SetHidden(not self.dispimg.IsHidden())
-    if show:
-      # TODO: cancel timer
-      self.disptext.Show()
-      self.dispimg.Hide()
-    else:
-      # TODO: set timer to return to text...
-      self.disptext.Hide()
-      self.dispimg.Show()
-
-  #----------------------------------------------------------------------------
-  def toggleLock(self, event):
-    print 'togglelock'
-    # TODO: replace this with:
-    #         def do_lock(self, event):
-    #           self.lock.state = self.lock.LOCKED
-    if self.lock.state & self.lock.LOCKED:
-      self.lock.state &= ~ self.lock.LOCKED
-    else:
-      self.lock.state |= self.lock.LOCKED
+  def showDisplayCamera(self, show):
+    self.disp_thread.running = show
+    # TODO: set/cancel timers for showing camera vs. text, etc...
+    self.disptext.Show(not show)
+    self.dispimg.Show(show)
 
   #----------------------------------------------------------------------------
   def do_leave(self, event):
@@ -160,24 +221,34 @@ class App(wx.App):
   #----------------------------------------------------------------------------
   def lockChanged(self, **kw):
     msg = _('N/A')
+
+    # self.btnleave.SetTransparent(150)
+    # self.btnunlock.SetTransparent(150)
+    # self.btnlock.SetTransparent(150)
+    # self.btnplock.SetTransparent(150)
+
     if self.lock.tostate is not None:
       # TODO: what about self.lock.ONCLOSE ?...
+      # todo: what about transparency?...
       if self.lock.tostate & self.lock.LOCKED:
         msg = _('Locking...')
       else:
         msg = _('Unlocking...')
     else:
+      # todo: what about transparency on self.lock.ONCLOSE?...
       if self.lock.state & self.lock.LOCKED:
         if self.lock.state & self.lock.PERMANENT:
           msg = _('PERMA-\nLOCKED')
+          # self.btnplock.SetTransparent(255)
         else:
           msg = _('LOCKED')
+          # self.btnlock.SetTransparent(255)
       else:
         msg = _('UNLOCKED')
+        # self.btnunlock.SetTransparent(255)
     self.disptext.SetLabel(msg)
-    self.showDisplayText(True)
+    self.showDisplayCamera(False)
     self.disppanel.Layout()
-
 
 #------------------------------------------------------------------------------
 # end of $Id$
